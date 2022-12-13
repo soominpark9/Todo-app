5bce7175004f0449f85376889bd59f0bd1c35805
park <goslwjddl@naver.com>
2022-12-13 10:21:47 +0900

    feat: Todo 추가 기능 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,57 @@
+import { useState, useRef } from "react";
+//사실 디비에서 데이터가져올거면 useRef 쓸 필요가 없다는...........?ㅎ
 import Todo from "./components/Todo";
-import { useState } from "react";
-
+import AddTodo from "./components/AddTodo";
 
 const App = () => {
   const [todoItems, setTodoItems] = useState([
     {
       id: 1,
-      title: 'My Todo1',
+      title: "My Todo1",
       done: false,
     },
     {
       id: 2,
-      title: 'My Todo2',
+      title: "My Todo2",
       done: false,
     },
     {
       id: 3,
-      title: 'My Todo3',
+      title: "My Todo3",
       done: true,
     },
   ]);
 
+  const todoId = useRef(4)
+  //todoId를 useRef로 관리하기 위해서 썼는데 useRef는 dom요소 접근뿐만아니라 
+  // 변수로도 사용할 수 있어서 여기선 위에 예시 배열에 id가 3번까지만있어서 4번 부터 추가해야해서 
+  // 변수를 4부터 시작했다
+
+
+
+  // AddTodo 컴포넌트는 상위 컴포넌트(App)의 todoItems(state)에 접근 불가능
+  // 상위 컴포넌트(App)은 AddTodo 컴포넌트 접근 가능
+  // => App 컴포넌트에 addItem() 함수를 정의하고, 해당 함수를 AddTodo props로 넘겨야 함
+  const addItem = (newItem) => {
+    //newItem 에 넘겨온 todoItem (title)이 여기로 와서 이제 title을 여기서도 찍을 수 있다>
+    //매개변수 newItem을 적어줌
+    // ...
+    //newItem - {id:?, title:?, done:false}
+    //id는 길이계산해서 title addtodo로 받아온것으로 하고 done은 기본적인걸로
+    //setTodoItems() 로 보내주기??
+
+    newItem.id = todoId.current++; //key를 위한 id설정
+    //여기선 todoId를 useRef로 관리랄 하고 있어서 useRef는 current에 접근 해야 쓸수 있기 때문에 .current를 썻고
+    //++는 id값이 하나씩 증가해야하기때문에 근데 db쓰면 사실상 이부분 다 필요없다함 지금 여기선 프론트라 보여주기 위해서 
+    newItem.done = false; //done 초기화
+    //기존 todoItems를 유지하고, 새로운 newItem을 추가
+    setTodoItems([...todoItems,newItem])
+  };
   return (
+    // AddTodo 컴포넌트 추가
     <div className="App">
+      <AddTodo addItem={addItem}/>
+      {/*AddTodo 에 props로 additem을 넘겨준다  */}
       {todoItems.map((item) => {
         // console.log(item); // {id: 1, title: 'My Todo1', done: false}
         return <Todo key={item.id} item={item} />;
---
+++ client/src/components/AddTodo.js
@@ -0,0 +1,42 @@
+//1. 함수형 컴포넌트
+//2. input과 button을 가짐
+import { useState } from "react";
+const AddTodo = (props) => {
+  const { addItem } = props;
+//const { addItem } = props; 안쓰고 간략하게 하려면 위의props란에 바로 addItem을 넣으면됨
+
+  //사용자 입력을 저장할 객체
+  //(id, title, done에 대한 정보를 저장해야해서 객체 형태로 !!)
+  const [todoItem, setTodoItem] = useState({
+    title: "",
+  });
+  //props 받아와야함
+  //상태가 필요함
+
+  const onButtonClick = () => {
+    //props로 받아온 additem 함수 실행
+    addItem(todoItem); //{title : 'input입력값'}
+    //newItem으로 넘겨줌
+    setTodoItem({ title: "" });
+  };
+  //enter키 입력시 아이디 추가
+  const onEnterKeyPress = (e) => {
+    if (e.key == 'Enter') {
+      onButtonClick();
+    }
+  };
+  return (
+    <div className="AddTodo">
+      <input
+        type="text"
+        placeholder="Add your new Todo"
+        value={todoItem.title}
+        onChange={(e) => setTodoItem({ title: e.target.value })}
+        onKeyPress={onEnterKeyPress}
+      />
+      <button onClick={onButtonClick}>ADD</button>
+    </div>
+  );
+};
+
+export default AddTodo;
