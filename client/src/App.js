dcd0b5521bff1e2e5aa741be3f499105083d5b0f
park <goslwjddl@naver.com>
2022-12-13 12:01:21 +0900

    feat: Todo 삭제 기능 구현

--- client/src/App.js
+++ client/src/App.js
@@ -22,12 +22,10 @@ const App = () => {
     },
   ]);
 
-  const todoId = useRef(4)
-  //todoId를 useRef로 관리하기 위해서 썼는데 useRef는 dom요소 접근뿐만아니라 
-  // 변수로도 사용할 수 있어서 여기선 위에 예시 배열에 id가 3번까지만있어서 4번 부터 추가해야해서 
+  const todoId = useRef(4);
+  //todoId를 useRef로 관리하기 위해서 썼는데 useRef는 dom요소 접근뿐만아니라
+  // 변수로도 사용할 수 있어서 여기선 위에 예시 배열에 id가 3번까지만있어서 4번 부터 추가해야해서
   // 변수를 4부터 시작했다
-
-
 
   // AddTodo 컴포넌트는 상위 컴포넌트(App)의 todoItems(state)에 접근 불가능
   // 상위 컴포넌트(App)은 AddTodo 컴포넌트 접근 가능
@@ -39,22 +37,37 @@ const App = () => {
     //newItem - {id:?, title:?, done:false}
     //id는 길이계산해서 title addtodo로 받아온것으로 하고 done은 기본적인걸로
     //setTodoItems() 로 보내주기??
-
     newItem.id = todoId.current++; //key를 위한 id설정
     //여기선 todoId를 useRef로 관리랄 하고 있어서 useRef는 current에 접근 해야 쓸수 있기 때문에 .current를 썻고
-    //++는 id값이 하나씩 증가해야하기때문에 근데 db쓰면 사실상 이부분 다 필요없다함 지금 여기선 프론트라 보여주기 위해서 
+    //++는 id값이 하나씩 증가해야하기때문에 근데 db쓰면 사실상 이부분 다 필요없다함 지금 여기선 프론트라 보여주기 위해서
     newItem.done = false; //done 초기화
     //기존 todoItems를 유지하고, 새로운 newItem을 추가
-    setTodoItems([...todoItems,newItem])
+    setTodoItems([...todoItems, newItem]);
+  };
+  //전체 Todo 리스트(todoItems)는 App 컴포넌트에서 관리하고 있으므로
+  //delete() 함수는 App 컴포넌트에 작성해야함
+  const deleteItem = (targetItem) => {
+    //filter 매서드 이용해서 선택된 얘만 제외하고 새로운 배열로 저장되게
+    //filter 매서드는 참인 애들만 남겨준다
+    const result = todoItems.filter(
+      (todoItem) => targetItem.id !== todoItem.id
+    );
+    //todoItems -> 전체 todoItem
+    //filter(todoItem) -> todoItems의 각각의 원소
+    //filter로 거르고 참 값만 보여준단 뜻
+    //대신 (todoItem) =>여기가 다른걸로 바뀌면 !== todoItem.id 여기도 todoItem이 아닌 바뀐 값을 넣어줘야함
+    setTodoItems(result);
+    console.log(targetItem);
   };
   return (
     // AddTodo 컴포넌트 추가
     <div className="App">
-      <AddTodo addItem={addItem}/>
+      <AddTodo addItem={addItem} />
       {/*AddTodo 에 props로 additem을 넘겨준다  */}
       {todoItems.map((item) => {
         // console.log(item); // {id: 1, title: 'My Todo1', done: false}
-        return <Todo key={item.id} item={item} />;
+        return <Todo key={item.id} item={item} deleteItem={deleteItem} />;
+        //deleteItem은 todo안에 있으니 여기서 보내줘야한다
         //이제 여기서 Todo.js로 props를 넘겨준다 여기서 props는 item이다
       })}
     </div>
--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,15 +1,22 @@
 //1. 함수형 컴포넌트
 //2. input(checkbox) 와 label을 랜더링하는 컴포넌트
 //3. App (부모 컴포넌트)에서 Todo (자식 컴포넌트) 1개를 랜더링한다.
+import {useState} from 'react';
 
-const Todo = ({item}) => {
-    //APP.JS에서 props를 가져옴
-//console.log(items) -{id:1, title: 'todo1', true}
-  // const {items} = props (위에 items를 넣어서 이거안써줘도됨)
+const Todo = ({ item, deleteItem }) => {
+  //APP.JS에서 props를 가져옴
+  //console.log(items) -{id:1, title: 'todo1', true}
+  // const {items,deleteItem} = props 하고 원래 (props) 해줘야하는데
+  // props자리에 ( items,deleteItem를 넣어서 구조분해 할당 안해도됨
+
   const { id, title, done } = item;
+  const[todoItem, setItem] = useState(item);
   //const { id, items, done } = items; 얘를 여기서 구조 분해 할당을 안하면
   //밑에서 todo${item.id} 이렇게 쓰면된다
-
+  const onDeleteBtnClick = () => {
+    deleteItem(todoItem);
+    //delete클릭할때 마다 현재 삭제될게 뭔지 알려줌 todoItem 얘임
+  };
   return (
     <div className="Todo">
       <input
@@ -20,6 +27,7 @@ const Todo = ({item}) => {
         defaultChecked={done}
       />
       <label htmlFor={`todo${id}`}>{title}</label>
+      <button onClick={onDeleteBtnClick}>DELETE</button>
       {/* 여기서 Mytodo 가 데이터 받는 곳 -> react에서 데이터는 부모에서 -> 자식으로 흐르기때문에 부모요소인 app.js에 데이터를 받는다 */}
     </div>
   );
